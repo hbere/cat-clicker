f0de3188c0e0bca0e4583aae1fd9cebdf64e24f9
Hal Beresford <1526859+hbere@users.noreply.github.com>
2018-10-20 17:44:07 -0400

    Comments out unneccessary code in app.js

--- js/app.js
+++ js/app.js
@@ -45,28 +45,28 @@ document.body.innerHTML = '';
 
 var nums = [1,2,3];
 
-// Let's loop over the numbers in our array
-for (var i = 0; i < nums.length; i++) {
+// // Let's loop over the numbers in our array
+// for (var i = 0; i < nums.length; i++) {
 
-    // This is the number we're on...
-    var num = nums[i];
+//     // This is the number we're on...
+//     var num = nums[i];
 
-    // We're creating a DOM element for the number
-    var elem = document.createElement('div');
-    elem.textContent = num;
+//     // We're creating a DOM element for the number
+//     var elem = document.createElement('div');
+//     elem.textContent = num;
 
-    // ... and when we click, alert the value of `num`
-    elem.addEventListener('click', (function(numCopy) {
-        return function() {
-            alert(numCopy);
-        };
-    })(num));
+//     // ... and when we click, alert the value of `num`
+//     elem.addEventListener('click', (function(numCopy) {
+//         return function() {
+//             alert(numCopy);
+//         };
+//     })(num));
 
-    // // ... because this version does not work:
-    //     // ... and when we click, alert the value of `num`
-    //     elem.addEventListener('click', function() {
-    //       alert(num);
-    //   });
+//     // // ... because this version does not work:
+//     //     // ... and when we click, alert the value of `num`
+//     //     elem.addEventListener('click', function() {
+//     //       alert(num);
+//     //   });
 
-    document.body.appendChild(elem);
-};
+//     document.body.appendChild(elem);
+// };
