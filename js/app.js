3da4086cb4f3ce2a23e6a0e2d2d06a5f01b9c31c
Hal Beresford <1526859+hbere@users.noreply.github.com>
2018-10-21 19:15:22 -0400

    Gets single cat clicker working

--- js/app.js
+++ js/app.js
@@ -2,24 +2,46 @@
 // Now using MVO (a.k.a. MVC a.k.a. MV*)
 // October 21, 2018
 
+
+// Elements
+const CAT_NAME = document.getElementById("catName");
+const CAT_CLICKS = document.getElementById("catClicks");
+const CAT_IMG = document.getElementById("catImg");
+let CURRENT_CAT_INDEX = 0;
+
+
+// MVO
 let model = {
+  // Initiatlizes the data model
   init: function () {
     if (!localStorage.cats) {
       localStorage.cats = JSON.stringify([]);
     }
   },
+
+  // Adds a cat
   add: function (cat) {
     let data = JSON.parse(localStorage.cats);
     data.push(cat);
     localStorage.cats = JSON.stringify(data);
   },
-  getAllCats: function () {
-    return JSON.parse(localStorage.cats);
+
+  // Returns a cat for use in the octopus
+  getCat: function (index) {
+    let data = JSON.parse(localStorage.cats);
+    return data[index];
+  },
+
+  // Increments a cat's clicks
+  incrementClicks: function (index) {
+    let data = JSON.parse(localStorage.cats);
+    data[index].clicks += 1;
+    localStorage.cats = JSON.stringify(data);
   }
 };
 
 let octopus = {
-  currentCatIndex: 0,
+  // Adds a new cat
   addNewCat: function (catName, src, altText) {
     model.add({
       name: catName,
@@ -28,51 +50,41 @@ let octopus = {
       altText: altText
     });
   },
+
+  // Adds all cats - populates the database
   addAllCats: function () {
     this.addNewCat(
       "Ghost",
       "./img/cat1.jpg",
       "Cat. Thanks to NeONBRAND for the photo via https://unsplash.com/photos/UETa8mfu38k."
     );
-    this.addNewCat(
-      "Candy",
-      "./img/cat2.jpg",
-      "Cat. Thanks to Cat Mapper (Max Ogden) for the photo via https://unsplash.com/photos/EcsCeS6haJ8."
-    );
-    this.addNewCat(
-      "Lozada",
-      "./img/cat3.jpg",
-      "Cat. Thanks to Timothy Meinberg for the photo via https://unsplash.com/photos/b079C-_tUbM."
-    );
-    this.addNewCat(
-      "Pearl",
-      "./img/cat4.jpg",
-      "Cat. Thanks to Mikhail Vasilyev for the photo via https://unsplash.com/photos/NodtnCsLdTE."
-    );
-    this.addNewCat(
-      "Irma",
-      "./img/cat5.jpg",
-      "Cat. Thanks to Kari Shea for the photo via https://unsplash.com/photos/eMzblc6JmXM."
-    );
   },
-  clickOnCat: function () {
-    let cats = model.getAllCats();
-    this.clicks += 1;
-    CAT_CLICKS.innerText = this.clicks;
-  },
-  init: function () {
-    let cats = model.getAllCats();
 
+  // Clicks on a cat
+  clickOnCat: function () {
+    model.incrementClicks(CURRENT_CAT_INDEX);
+    view_cat.render(model.getCat(CURRENT_CAT_INDEX));
+  },
+
+  // Initiatlizes the octopus
+  init: function () {
     // Initiatize model & add data
     model.init();
     this.addAllCats();
-
-    // Initialize menu
-    view_menu.init();
+    let cat = model.getCat(CURRENT_CAT_INDEX);
 
     // Initialize cat image
-    let currentCat = cats[this.currentCatIndex];
-    view_cat.init(currentCat);
+    view_cat.init(cat);
+  }
+}
+
+let view_cat = {
+  init: function (currentCat) {
+    // Set values
+    CAT_NAME.innerText = currentCat.name;
+    CAT_CLICKS.innerText = currentCat.clicks;
+    CAT_IMG.src = currentCat.src;
+    CAT_IMG.alt = currentCat.altText;
 
     // Add cat image event listener
     CAT_IMG.addEventListener(
@@ -82,38 +94,8 @@ let octopus = {
       },
       false
     );
-  }
-}
-
-let view_menu = {
-  init: function () {
-    let cats = model.getAllCats();
-
-    // Draw menu
-    for (cat of cats) {
-      let myHTML = `<li>${cat.name}</li>`;
-      document.querySelector("menu").innerHTML += myHTML;
-    }
-
-    // Add menu event listeners
-    cats.forEach((cat, index) => {
-      let menuItem = document.querySelectorAll("li")[index];
-      // console.log(index);
-      menuItem.addEventListener("click", function () {
-        currentCat = cats[index];
-        currentCat.load();
-      });
-    });
-  }
-};
-
-let view_cat = {
-  init: function (currentCat) {
-    // Elements
-    const CAT_NAME = document.getElementById("catName");
-    const CAT_CLICKS = document.getElementById("catClicks");
-    const CAT_IMG = document.getElementById("catImg");
-
+  },
+  render: function (currentCat) {
     // Set values
     CAT_NAME.innerText = currentCat.name;
     CAT_CLICKS.innerText = currentCat.clicks;
@@ -121,6 +103,7 @@ let view_cat = {
     CAT_IMG.alt = currentCat.altText;
   }
 };
+
 
 localStorage.clear();
 octopus.init();
